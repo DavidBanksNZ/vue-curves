c3bf736ec7b7661044e6a0c1c15176928a36863b
David Banks <davidgbanks@gmail.com>
2017-07-30 15:17:23 +1200

    minor code cleanup

--- src/web-worker/worker.js
+++ src/web-worker/worker.js
@@ -17,11 +17,8 @@ onmessage = e => {
 				points = Hilbert.calculatePoints(e.data);
 				break;
 			case 'Koch':
-				e.data.opts = {...e.data.opts, full: true};
-				points = Koch.calculatePoints(e.data);
-				break;
 			case 'Cesàro':
-				e.data.opts = {...e.data.opts, full: false};
+				e.data.opts = {...e.data.opts, full: e.data.algorithm === 'Koch'};
 				points = Koch.calculatePoints(e.data);
 				break;
 			case 'Dragon':
